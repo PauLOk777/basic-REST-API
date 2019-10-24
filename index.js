f8a6a04cc56ebf82915df446a56e010248c429bb
PauLOk777 <trocjuk2000@gmail.com>
2019-10-24 18:09:02 +0300

    Added file .html and modified index.js, modified .travis.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const http = require('http');
-const hostname = 'localhost';
-const port = 3000;
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hi there\n');
-});
-server.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+'use strict'
+
+function changeHr(obj) {
+	let tempColor = obj.colorChange.value;
+	let color = '';
+	for (let i = 0; i < tempColor.length; i++) {
+		if(tempColor[i] == 'C') break;
+		color += tempColor[i];
+	}
+	line.color = color;
+	line.width = `${obj.widthChange.value}%`;
+	line.size = obj.thickness.value;
+}
+
+function resetChanges(obj) {
+	line.color = 'blue';
+	line.width = '100%';
+	line.size = '1px';
+}
