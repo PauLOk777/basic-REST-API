6480282fea99e603a24f4ce0b3a81544f10d251c
PauLOk777 <trocjuk2000@gmail.com>
2019-10-25 16:29:51 +0300

    Modified index.js and travis, now we can automaticly deploy the project.

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const app = express();
-app.use(express.logger());
+var express = require('express');
+var app = express();
 
-app.get('/', function(request, response) {
-    response.sendFile('/app/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log('Listening on ' + port);
-});
+app.listen(8080);
+console.log('Server has started!');
