941c2e170a7c563b0e3071b45ed0b77742858206
PauLOk777 <trocjuk2000@gmail.com>
2019-10-25 16:41:01 +0300

    Modified index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-'use strict'
+'use strict';
 
-var express = require('express');
-var app = express();
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(express.static(__dirname));
+app.set("view engine", "hbs");
+app.get("/", urlencodedParser, function(request, response) {
+  response.sendFile(__dirname + "/index.html");
 });
-
-app.listen(8080);
-console.log('Server has started!');
+app.listen(PORT);
