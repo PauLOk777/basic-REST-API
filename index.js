cd8cf9fe64aa461102198f0a07147b721e9a8f07
PauLOk777 <trocjuk2000@gmail.com>
2019-10-25 16:48:09 +0300

    Modified html file.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static(__dirname));
-app.set("view engine", "hbs");
-app.get("/", urlencodedParser, function(request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.set('view engine', 'hbs');
+app.get('/', urlencodedParser, function(request, response) {
+    response.sendFile(__dirname + '/index.html');
 });
 app.listen(PORT);
