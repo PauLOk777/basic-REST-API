b92542534e3d189f2d1e99845490552d91abc64e
PauLOk777 <trocjuk2000@gmail.com>
2019-10-25 13:51:14 +0300

    Added Procfile for heroku, modified travis.yml and elsintrc.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-'use strict'
+'use strict';
 
-const express = require("express");
+const express = require('express');
 const app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.sendFile('index.html');
+    response.sendFile('index.html');
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log('Listening on ' + port);
 });
