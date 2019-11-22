41e06eaeaf7d870b4b9531568658d4d6b96fcac7
PauLOk777 <trocjuk2000@gmail.com>
2019-11-23 00:02:41 +0200

    All handlers was created for REST API, now we can realized it, database was connected, some bugs was fixed.

--- index.js
+++ index.js
@@ -8,39 +8,46 @@ const app = express();
 const Schema = mongoose.Schema;
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-const userSchem = new Schema({
-    name: String,
-    age: Number,
-});
-
+const userSchem = new Schema({ name: String, age: Number }, { versionKey: false });
 const User = mongoose.model('User', userSchem);
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-    mongoose.connect(
-        'mongodb://localhost:27017/usersdb',
-        { useNewUrlParser: true },
-        err => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log('Connected!!!');
-        }
-    );
-    app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
-    res.sendFile('index.html');
+mongoose.connect('mongodb://localhost:27017/usersdb', { useNewUrlParser: true, useUnifiedTopology: true }, function(err){
+    if (err) console.error(err);
+    console.log('>>> Database was connected');
+   	app.listen(PORT, "127.0.0.1", function() {
+		console.log('>>> Server is running!');
+	}); 
 });
 
-app.get('/api/users', (req, res) => {});
+app.get('/', (req, res) => {
+   	res.sendFile('index.html');
+});
 
-app.get('/api/users/:id', (req, res) => {});
+app.get('/api/users', (req, res) => {
+	res.send('Hi');
+	console.log('Get all users');
+});
 
-app.post('/api/users', (req, res) => {});
+app.get('/api/users/:id', (req, res) => {
+	console.log('Get by id');
+});
 
-app.put('/api/users', (req, res) => {});
+app.post('/api/users', urlencodedParser, (req, res) => {
+	console.log('Add new user');
+});
 
-app.delete('/api/users:id', (req, res) => {});
+app.put('/api/users', urlencodedParser, (req, res) => {
+	console.log('Update user');
+});
+
+app.delete('/api/users:id', (req, res) => {
+	console.log('Delete by id');
+});
+
+process.on('SIGINT', () => {
+	process.exit(0);
+});
