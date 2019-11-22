81c14751cb914d93a78cc3bda8578a13d4820011
PauLOk777 <trocjuk2000@gmail.com>
2019-11-22 22:23:10 +0200

    Created all handlers for our API.

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const app = express();
 const Schema = mongoose.Schema;
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-
 const userSchem = new Schema({
-	name: String,
-	age: Number
+    name: String,
+    age: Number,
 });
 
 const User = mongoose.model('User', userSchem);
@@ -21,34 +20,27 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-	mongoose.connect('mongodb://localhost:27017/usersdb',
- 					{ useNewUrlParser: true }, function(err) {
-	 	if(err) {
-	 		console.error(err);
-	 		return;
-	 	}
- 		console.log('Connected!!!');
-	});
-	app.listen(PORT, () =>  console.log(`Server running at port: ${PORT}`));
-	res.sendFile('index.html');
+    mongoose.connect(
+        'mongodb://localhost:27017/usersdb',
+        { useNewUrlParser: true },
+        function(err) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log('Connected!!!');
+        }
+    );
+    app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
+    res.sendFile('index.html');
 });
 
-app.get('/api/users', (req, res) => {
+app.get('/api/users', (req, res) => {});
 
-});
+app.get('/api/users/:id', (req, res) => {});
 
-app.get('/api/users/:id', (req, res) => {
+app.post('/api/users', (req, res) => {});
 
-});
+app.put('/api/users', (req, res) => {});
 
-app.post('/api/users', (req, res) => {
-
-});
-
-app.put('/api/users', (req, req) => {
-
-});
-
-app.delete('/api/users:id', (req, req) => {
-	
-});
+app.delete('/api/users:id', (req, res) => {});
